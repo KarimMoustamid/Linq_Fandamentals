208d1a6dfe95acf31e46a457fd43cc47bef4ddd2
karimmoustamid <karim.moustamid@agilethought.com>
2024-09-15 10:51:06 -0600

    Update method calls in Program.cs to use new column retrieval methods
    Replaced legacy method calls in `Program.cs` with new ones to retrieve a single column using `GetSingleColumnMethod`. Refactored code in `SamplesViewModel.cs` to properly utilize indentation and updated surrounding logic. Assembly information and source link files were also modified automatically as part of the build process.

--- Select/Program.cs
+++ Select/Program.cs
@@ -3,6 +3,8 @@ using LINQSamples;
 SamplesViewModel vm = new SamplesViewModel();
 
 // List<Product> products = vm.GetAllQuery();
-List<Product> products = vm.GetAllMethod();
+// List<Product> products = vm.GetAllMethod();
+// List<string> products = vm.GetSingleColumnQuery();
+List<string> products = vm.GetSingleColumnMethod();
 
 vm.Display(products);
