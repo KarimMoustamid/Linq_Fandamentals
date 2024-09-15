5436c1a981b681cd31aa9890151801a6f1025f87
karimmoustamid <karim.moustamid@agilethought.com>
2024-09-15 10:02:51 -0600

    Add project reference to LINQSamples.DataLayer
    Integrated LINQSamples.DataLayer project reference in Select. This update changes the Program.cs to use ProductRepository from the new project, ensuring a more modular and maintainable structure.

--- Select/Program.cs
+++ Select/Program.cs
@@ -1,11 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-List<Product> products = new List<Product>
-{
-    new Product { Id = 1, Name = "Product 1", Price = 12.99m },
-    new Product { Id = 2, Name = "Product 2", Price = 25.50m },
-    new Product { Id = 3, Name = "Product 3", Price = 32.00m }
-};
+using LINQSamples;
+List<Product> products =  ProductRepository.GetAll();
+
 
 decimal Min = decimal.MaxValue;
 
@@ -20,11 +17,5 @@ decimal Min = decimal.MaxValue;
 // Console.WriteLine($"Max Price: {Min}");
 
 
-decimal value = (from prod in products select prod.Price).Min();
+decimal value = (from prod in products select prod.ListPrice).Min();
 Console.WriteLine($"Price: {value}");
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-}
